9c28497fcf7b74a33833d19e5c4d40fe2a11845d
Glen <glen@jamdirect.ca>
2016-10-07 08:47:12 -0400

    Middle of changing maps for comtact section

--- contact.php
+++ contact.php
@@ -29,22 +29,35 @@
     </div>    
 
     <div class="sheppard">
-        <img src="img/45-sheppard.jpg">
+
+        <div class="left">
+          <img src="img/45-sheppard.jpg">
+          
+        </div>
+        
+        <div class="right">
+          <iframe class="map-1" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2881.5365417379867!2d-79.4080591!3d43.7617201!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b2d669042da83%3A0x783096e80d4374a6!2s45+Sheppard+Ave+E%2C+North+York%2C+ON+M2N+5W9!5e0!3m2!1sen!2sca!4v1414966534707" width="100%" height="200" frameborder="0" style="border:0"></iframe>   
+        </div>
+
     </div>
 
-    <iframe class="map-1" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d2881.5365417379867!2d-79.4080591!3d43.7617201!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b2d669042da83%3A0x783096e80d4374a6!2s45+Sheppard+Ave+E%2C+North+York%2C+ON+M2N+5W9!5e0!3m2!1sen!2sca!4v1414966534707" width="1000" height="400" frameborder="0" style="border:0"></iframe>
+    
 
     <div class="queen">
         <img src="img/220-queen.jpg">
+
+        <iframe class="map-2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2890.2318191352283!2d-79.71263950000002!3d43.580887499999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b41b95d2aa6e3%3A0x5d3b30332849dd45!2s220+Queen+St+S%2C+Mississauga%2C+ON+L5M+1L6!5e0!3m2!1sen!2sca!4v1416409708623" width="100%" frameborder="0" style="border:0"></iframe>
     </div> 
             
-    <iframe class="map-2" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2890.2318191352283!2d-79.71263950000002!3d43.580887499999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b41b95d2aa6e3%3A0x5d3b30332849dd45!2s220+Queen+St+S%2C+Mississauga%2C+ON+L5M+1L6!5e0!3m2!1sen!2sca!4v1416409708623" width="1000" height="400" frameborder="0" style="border:0"></iframe>
+  
 
     <div class="brant">
         <img src="img/460-brant.jpg">
+
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2902.4047349673738!2d-79.79938393004504!3d43.32671908607583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882c9e24dcd40e39%3A0x79993b96cf31beaf!2s460+Brant+St%2C+Burlington%2C+ON+L7R+4B6!5e0!3m2!1sen!2sca!4v1475802862190" width="100%" frameborder="0" style="border:0" allowfullscreen></iframe>
     </div> 
 
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2902.4047349673738!2d-79.79938393004504!3d43.32671908607583!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882c9e24dcd40e39%3A0x79993b96cf31beaf!2s460+Brant+St%2C+Burlington%2C+ON+L7R+4B6!5e0!3m2!1sen!2sca!4v1475802862190" width="1000" height="400" frameborder="0" style="border:0" allowfullscreen></iframe>
+    
            
 
 <?php include('inc/footer.php'); ?>
